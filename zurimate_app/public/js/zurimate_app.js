a1f0a1f7bb53a91079a87b0350fcd70a16ed3923
japheth-waswa <japhethwaswa@gmail.com>
2024-09-26 13:32:27 +0000

    added two test menus

--- zurimate_app/public/js/zurimate_app.js
+++ zurimate_app/public/js/zurimate_app.js
@@ -18,10 +18,15 @@ function addMenuItemsInSideMenu() {
         var publicSection = $('.standard-sidebar-section[data-title="Public"]');
         if (publicSection.length > 0) {
             if ($('.standard-sidebar-section .zurimate-menu-item-side-bar').length === 0) {
-                publicSection.append('<div class="is-draggable sidebar-item-container zurimate-menu-item-side-bar"item-is-hidden=0 item-name="Z Item"item-parent=""item-public=1><div class="desk-sidebar-item standard-sidebar-item"><a class=item-anchor href=/app/zurimate-item title="Z Item"><span class=sidebar-item-icon item-icon=tool><svg aria-hidden=true class="icon icon-md"><use href=#icon-tool></use></svg> </span><span class=sidebar-item-label>Z Item<span></span></span></a></div><div class="nested-container sidebar-child-item"></div></div>');
+                // publicSection.append('<div class="is-draggable sidebar-item-container zurimate-menu-item-side-bar"item-is-hidden=0 item-name="Z Item"item-parent=""item-public=1><div class="desk-sidebar-item standard-sidebar-item"><a class=item-anchor href=/app/zurimate-item title="Z Item"><span class=sidebar-item-icon item-icon=tool><svg aria-hidden=true class="icon icon-md"><use href=#icon-tool></use></svg> </span><span class=sidebar-item-label>Z Item<span></span></span></a></div><div class="nested-container sidebar-child-item"></div></div>');
+                publicSection.append(addSideMenu({ title: "Mavoko", uri: "/app/website" }));
+                publicSection.append(addSideMenu({ title: "Mvk Water", uri: "/app/mvk_water" }));
+                publicSection.append(addSideMenu({ title: "Mvk Units", uri: "/app/mvk_units" }));
             }
         }
-    } else {
-        console.log("Help dropdown not found.xx");
     }
+}
+
+function addSideMenu({ title, uri = "/app/website" }) {
+    return '<div class="is-draggable sidebar-item-container zurimate-menu-item-side-bar"item-is-hidden=0 item-name="' + title + '"item-parent=""item-public=1><div class="desk-sidebar-item standard-sidebar-item"><a class=item-anchor href="' + uri + '" title="' + title + '"><span class=sidebar-item-icon item-icon=tool><svg aria-hidden=true class="icon icon-md"><use href=#icon-tool></use></svg> </span><span class=sidebar-item-label>' + title + '<span></span></span></a></div><div class="nested-container sidebar-child-item"></div></div>';
 }
